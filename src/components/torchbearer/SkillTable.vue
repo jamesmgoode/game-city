e9f34f839199bb4d9949ca2c94315185920856bb
James Goode <james_goode@hotmail.co.uk>
2020-06-06 00:33:37 +0100

    SkillTable

---
+++ src/components/torchbearer/SkillTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <table>
+    <tbody>
+      <tr v-for="skillView in skills" :key="skillView.skill.id">
+        <td class="skill-name">{{ skillView.skill.name }}</td>
+        <td>{{ skillView.suggestedHelpSkills }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+// eslint-disable-next-line no-unused-vars
+import { SkillData } from "../../models/skill-data";
+
+@Component
+export default class SkillTable extends Vue {
+  @Prop() skills!: SkillData[];
+}
+</script>
+
+<style scoped>
+  .skill-name {
+    font-weight: bold;
+  }
+</style>
--- src/components/torchbearer/Torchbearer.vue
+++ src/components/torchbearer/Torchbearer.vue
@@ -4,43 +4,46 @@
       <h1>Torchbearer</h1>
       <div class="torchbearer-content">
         <h2>Torchbearer Skills</h2>
-        <table>
-          <tbody>
-            <tr v-for="skillView in skills" :key="skillView.skill.id">
-              <SkillRow v-bind:skillView="skillView" />
-            </tr>
-          </tbody>
-        </table>
+        <!-- <table>
+                    <tbody>
+                        <tr v-for="skillView in skills" :key="skillView.skill.id">
+                            <td class="skill-name">{{ skillView.skill.name }}</td>
+                            <td>{{ skillView.suggestedHelpSkills }}</td>
+                        </tr>
+                    </tbody>
+                </table> -->
+        <SkillTable v-bind:skills="skills" />
       </div>
     </div>
     <Loader v-else />
   </div>
 </template>
 
-<script>
-import * as axios from "axios";
-import Loader from "../Loader";
-import SkillRow from "./SkillRow";
+<script lang="ts">
+import axios from "axios";
+import Loader from "../Loader.vue";
+import SkillTable from "./SkillTable.vue";
+import { Component, Vue } from "vue-property-decorator";
+// eslint-disable-next-line no-unused-vars
+import { SkillData } from "../../models/skill-data";
 
-export default {
-  name: "Torchbearer",
-  components: { Loader, SkillRow },
-  data() {
-    return {
-      skills: []
-    };
-  },
+@Component({
+  components: { Loader, SkillTable },
+})
+export default class Torchbearer extends Vue {
+  skills: SkillData[] = [];
+
   mounted() {
     axios
       .get("https://www.unionlarp.co.uk/torchbearer/gettorchbearerskills")
-      .then(response => (this.skills = response.data));
+      .then((response) => (this.skills = response.data));
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.torchbearer-content {
-  text-align: left;
-}
+    .torchbearer-content {
+    text-align: left;
+    }
 </style>
